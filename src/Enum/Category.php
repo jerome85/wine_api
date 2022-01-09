bae5197facda41ec0c2225332a7413e168284010
jnourrain <jnourrain@attestra.com>
2022-01-09 10:05:50 -0500

    initial commit

---
+++ src/Enum/Category.php
@@ -0,0 +1,11 @@
+<?php
+namespace App\Enum;
+
+use MyCLabs\Enum\Enum;
+
+class Category extends Enum
+{
+    public const RED = 'red';
+    public const WHITE = 'white';
+    public const ROSE = 'rose';
+}
